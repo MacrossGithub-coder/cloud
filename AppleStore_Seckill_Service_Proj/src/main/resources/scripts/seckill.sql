53aa640873f6c1093f62999c5bbf35aa34cca7c2
i_zhangjiancheng <i_zhangjiancheng@weshareholdings.com>
2021-04-13 11:23:59 +0800

    Add test "Rest Seckill Database(include Redis database & mysql(sql script))"

---
+++ AppleStore_Seckill_Service_Proj/src/main/resources/scripts/seckill.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE seckill_order;
+TRUNCATE TABLE commodity_order;
+UPDATE commodity_seckill SET stock = 50 WHERE commodity_id = 21;
--- AppleStore_Seckill_Service_Proj/src/test/java/org/macross/AppleStore_Seckill_Service_Proj/AppleStoreSeckillServiceProjApplicationTests.java
+++ AppleStore_Seckill_Service_Proj/src/test/java/org/macross/AppleStore_Seckill_Service_Proj/AppleStoreSeckillServiceProjApplicationTests.java
@@ -1,13 +1,52 @@
 package org.macross.AppleStore_Seckill_Service_Proj;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.jupiter.api.Test;
+import org.macross.AppleStore_Seckill_Service_Proj.utils.YmlUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
 
 @SpringBootTest
 class AppleStoreSeckillServiceProjApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    @Qualifier("redisTemplateMasterSeckill")
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    void runSqlScript() throws Exception {
+        //Reset redis database
+        redisTemplate.opsForValue().set("21",50);
+
+        //Truncate table & reset commodity stock
+        Map<String, String> datasource = YmlUtils.getYmlByFileName("D:\\Users\\i_zhangjiancheng\\IdeaProjects\\cloud\\AppleStore_Seckill_Service_Proj\\src\\main\\resources\\application-dev.yml", "spring", "datasource");
+        Connection conn;
+        if (datasource!=null){
+            String url = datasource.get("spring.datasource.url");
+            String username = datasource.get("spring.datasource.username");
+            String password = datasource.get("spring.datasource.password");
+            try {
+                conn = DriverManager.getConnection(url,username,password);
+            } catch (SQLException throwable) {
+                throw new Exception(throwable.getMessage());
+            }
+        }else {
+            return;
+        }
+        ScriptRunner runner = new ScriptRunner(conn);
+        runner.setErrorLogWriter(null);
+        runner.setLogWriter(null);
+        runner.runScript(Resources.getResourceAsReader("scripts/seckill.sql"));
+        conn.close();
+        System.out.println("=======Success=======");
+    }
 
 }
